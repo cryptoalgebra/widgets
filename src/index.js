2f9673900d3228ac90ed031c0436c3d45530c691
MirYasa <gurevich.miron14@gmail.com>
2022-03-31 18:20:56 +0300

    create npm package

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-'use strict'
 
-const input = document.querySelector('.input')
-const title = document.querySelector('.title')
-const button100 = document.querySelector('.sto')
+(function (window) {
+    function calculator() {
 
-input.addEventListener('input', (e) => {
-    title.textContent = e.target.value
-})
-button100.addEventListener('click', () => {
-    title.textContent = '100'
-    input.value = 100
-})
+        const styles=`
+            <style>
+                .hkdfjsdf1 {
+                color: blue;
+                }
+            </style>
+        `
+
+        const html = `
+            <div>
+                <input/>
+                <h1 class="hkdfjsdf1">hi</h1>
+                <button>CLick</button>
+            </div>
+        `
+        const DOM = new DOMParser().parseFromString(html,'text/html')
+        const STYLE = new DOMParser().parseFromString(styles, 'text/html')
+
+        const wrapper = DOM.querySelector('div')
+        const input = DOM.querySelector('input')
+        const title = DOM.querySelector('h1')
+        const button100 = DOM.querySelector('button')
+
+        const style = STYLE.querySelector('style')
+
+        input.oninput = (e) => {
+            title.textContent = e.target.value
+        }
+
+        button100.onclick = () => {
+            title.textContent = '100'
+            input.value = '100'
+        }
+
+        return {
+            Calculator: wrapper,
+            CalculatorStyle: style,
+            buttonC: button100.onclick
+        }
+    }
+    window.Calculator = calculator()
+})(window)
