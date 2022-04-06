2b8256ca9fb4d67757d24d81c11f06f20828e434
MirYasa <gurevich.miron14@gmail.com>
2022-04-06 15:10:30 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
 const path = require('path');
+const MiniCss = require('mini-css-extract-plugin')
+const MinimizerCss = require('css-minimizer-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     devtool: 'eval',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js"
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: ['to-string-loader', "css-loader"],
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new MinimizerCss()
+        ]
+    },
+    // plugins: [new MiniCss()]
 }
